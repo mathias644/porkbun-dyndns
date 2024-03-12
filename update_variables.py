15a9a7771a9c306ddf1f56818179d4afa0bd68d5
Maciek Stankiewicz <90157206+mathias644@users.noreply.github.com>
2024-03-13 00:36:07 +0200

    Update update_variables.py

--- update_variables.py
+++ update_variables.py
@@ -1,7 +1,11 @@
 from pickle import dump
 
-## fill out each variable with your details
-secretapikey, apikey, id, domain = "","","",""
+# Fill each variable with your details
+
+secretapikey = ""
+apikey = ""
+id = ""
+domain = ""
 
 with open('variables', 'wb') as f:
     dump([secretapikey, apikey, id, domain], f)
