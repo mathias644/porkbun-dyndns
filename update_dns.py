2b31729bb56cb8958ba6c38ac77978909a659c61
Maciek Stankiewicz <90157206+mathias644@users.noreply.github.com>
2025-02-07 13:52:02 +0200

    updated new api domain for porkbun

--- update_dns.py
+++ update_dns.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 secretapikey = getenv('secretapikey')
 apikey = getenv('apikey')
-id = getenv('id')
 domain = getenv('domain')
 
 
@@ -14,10 +13,9 @@ myip = get("http://ipinfo.io/ip").text
 
 print(f"My external IP is: {myip}")
 
-
 print(f"Checking if the domain {domain} is currently set to {myip}")
 
-current_record_config = post(f"https://porkbun.com/api/json/v3/dns/retrieve/{domain}/{id}",json={"secretapikey":secretapikey, "apikey":apikey}).json()
+current_record_config = post(f"https://api.porkbun.com/api/json/v3/dns/retrieveByNameType/{domain}/A",json={"secretapikey":secretapikey, "apikey":apikey}).json()
 current_record_ip = current_record_config.get("records")[0].get("content")
 
 print(f"currently {domain} is set to {current_record_ip}")
@@ -25,6 +23,6 @@ print(f"currently {domain} is set to {current_record_ip}")
 if myip not in current_record_ip:
     print("Detected that current configuration is not correct; Attempting to update current configuration")
 
-    post(f"https://porkbun.com/api/json/v3/dns/edit/{domain}/{id}", json={"secretapikey":secretapikey, "apikey":apikey, "type": "A", "content": myip})
+    post(f"https://api.porkbun.com/api/json/v3/dns/editByNameType/{domain}/A", json={"secretapikey":secretapikey, "apikey":apikey, "content": myip})
 
     print("Configuration updated.")
